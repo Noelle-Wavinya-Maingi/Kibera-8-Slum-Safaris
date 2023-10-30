816bfd8d2802695355cf6e74383e32d34c8ca708
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-10-30 20:00:45 +0300

    Added more seed data for the tables and added relationships to the models in models.py file.

--- server/myapp/models.py
+++ server/myapp/models.py
@@ -28,6 +28,10 @@ class User(db.Model):
     password = Column(String(60), nullable=False)
     role = Column(String(20), nullable=False)
 
+    # Relationships with other tables
+    donations = relationship("Donation", back_populates="donor")
+    tours = relationship("Tours", back_populates="user")
+
     # Constructor to initialize a new user
     def __init__(self, username, email, password, role):
         self.username = username
@@ -50,10 +54,12 @@ class User(db.Model):
             raise ValueError("Email is required")
         return email
 
+    # String representation of the User object
     def __repr__(self):
-        return f"User(id = {self.id}, username = {self.username}, email = {self.email}, role = {self.role})"
+        return f"User(id={self.id}, username={self.username}, email={self.email}, role={self.role})"
 
 
+# Define the Organization model
 class Organization(db.Model):
     __tablename__ = "organizations"
 
@@ -66,18 +72,23 @@ class Organization(db.Model):
     password = Column(String(60))
     timestamp = Column(DateTime, default=datetime.utcnow)
 
+    # Relationships with other tables
     beneficiaries = relationship(
         "Beneficiary",
         secondary="beneficiaries_organizations",
         back_populates="organizations",
     )
+    donations = relationship("Donation", back_populates="organization")
+    stories = relationship("Story", back_populates="organization")
 
+    # Validation method for the email field
     @validates("email")
     def validate_email(self, key, email):
         if not email:
             raise ValueError("Email is required")
         return email
 
+    # Method to approve the organization's registration
     def approve_request(self):
         if not self.status:
             self.status = True
@@ -88,6 +99,7 @@ class Organization(db.Model):
             return True
         return False
 
+    # Method to reject the organization's registration with a reason
     def reject_request(self, reason):
         if not self.status:
             self.status = False
@@ -96,11 +108,13 @@ class Organization(db.Model):
             return True
         return False
 
+    # Method to generate a temporary password
     def generate_temp_password(self):
         password_characters = string.ascii_letters + string.digits
         temp_password = "".join(secrets.choice(password_characters) for _ in range(6))
         return temp_password
 
+    # Method to send an email with the temporary password
     def send_password_email(self, temp_password):
         msg = Message(
             "Organization Registration Approved",
@@ -110,6 +124,7 @@ class Organization(db.Model):
         msg.body = f"Your organization registration has been approved. Here is your login temporary password: {temp_password}"
         mail.send(msg)
 
+    # Method to send an email with the rejection reason
     def send_rejection_email(self, reason):
         msg = Message(
             "Organization Registration Rejected",
@@ -125,10 +140,12 @@ class Organization(db.Model):
         self.description = description
         self.email = email
 
+    # String representation of the Organization object
     def __repr__(self):
         return f"Organization(id={self.id}, name={self.name}, description={self.description}, status={self.status})"
 
 
+# Define the Donation model
 class Donation(db.Model):
     __tablename__ = "donations"
 
@@ -141,9 +158,11 @@ class Donation(db.Model):
     next_recurrence_date = Column(Date)
     created_at = Column(DateTime, default=datetime.utcnow)
 
+    # Relationships with other tables
     donor = relationship("User", back_populates="donations")
     organization = relationship("Organization", back_populates="donations")
 
+    # Constructor to initialize a new donation
     def __init__(
         self, amount, donor_id, organization_id, is_anonymous, recurrence_interval=None
     ):
@@ -154,6 +173,7 @@ class Donation(db.Model):
         self.recurrence_interval = recurrence_interval
         self.next_recurrence_date = self.calculate_next_recurrence_date()
 
+    # Method to calculate the next recurrence date
     def calculate_next_recurrence_date(self):
         if self.recurrence_interval == "monthly":
             return datetime.utcnow() + timedelta(days=30)
@@ -162,6 +182,7 @@ class Donation(db.Model):
         else:
             return None
 
+    # Method to process recurring donations
     def process_recurring_donations(self):
         current_date = datetime.utcnow()
         if self.next_recurrence_date and current_date >= self.next_recurrence_date:
@@ -177,6 +198,7 @@ class Donation(db.Model):
             db.session.add(new_donation)
             db.session.commit()
 
+    # Method to send a notification message
     def send_notification(self, message):
         msg = Message(
             "Donation Notification",
@@ -187,6 +209,7 @@ class Donation(db.Model):
         mail.send(msg)
 
 
+# Define the Beneficiary model
 class Beneficiary(db.Model):
     __tablename__ = "beneficiaries"
 
@@ -196,22 +219,27 @@ class Beneficiary(db.Model):
     gender = Column(String(10))
     address = Column(String(255))
 
+    # Relationships with other tables
     organizations = relationship(
         "Organization",
         secondary="beneficiaries_organizations",
         back_populates="beneficiaries",
     )
+    inventory = relationship("Inventory", back_populates="beneficiary")
 
+    # Constructor to initialize a new beneficiary
     def __init__(self, name, age=None, gender=None, address=None):
         self.name = name
         self.age = age
         self.gender = gender
         self.address = address
 
+    # String representation of the Beneficiary object
     def __repr__(self):
         return f"Beneficiary(id={self.id}, name={self.name}, age={self.age}, gender={self.gender}, address={self.address})"
 
 
+# Define the association table for beneficiaries and organizations
 beneficiaries_organizations = db.Table(
     "beneficiaries_organizations",
     Column("beneficiary_id", Integer, ForeignKey("beneficiaries.id")),
@@ -219,6 +247,7 @@ beneficiaries_organizations = db.Table(
 )
 
 
+# Define the Story model
 class Story(db.Model):
     __tablename__ = "stories"
 
@@ -228,20 +257,24 @@ class Story(db.Model):
     created_at = Column(DateTime)
     organization_id = Column(Integer, ForeignKey("organizations.id"), nullable=False)
 
-    organization = db.relationship("Organization", back_populates="stories")
+    # Relationship with the Organization table
+    organization = relationship("Organization", back_populates="stories")
 
+    # Constructor to initialize a new story
     def __init__(self, title, content, published_at, organization_id):
         self.title = title
         self.content = content
         self.published_at = published_at
         self.organization_id = organization_id
 
+    # String representation of the Story object
     def __repr__(self):
         return (
             f"Story(id={self.id}, title={self.title}, published_at={self.published_at})"
         )
 
 
+# Define the Inventory model
 class Inventory(db.Model):
     __tablename__ = "inventories"
 
@@ -250,24 +283,30 @@ class Inventory(db.Model):
     quantity = Column(Integer, nullable=False)
     beneficiary_id = Column(Integer, ForeignKey("beneficiaries.id"), nullable=False)
 
+    # Relationship with the Beneficiary table
     beneficiary = relationship("Beneficiary", back_populates="inventory")
 
+    # Validation method for the name field
     @validates("name")
     def validate_name(self, key, name):
         if not name:
             raise ValueError("Name is required")
         return name
 
+    # Constructor to initialize a new inventory item
     def __init__(self, name, quantity, beneficiary_id):
         self.name = name
         self.quantity = quantity
         self.beneficiary_id = beneficiary_id
 
+    # String representation of the Inventory object
     def __repr__(self):
         return f"Inventory(id={self.id}, name={self.name}, quantity={self.quantity}, beneficiary_id={self.beneficiary_id})"
-    
+
+
+# Define the Tours model
 class Tours(db.Model):
-    __tablename__ = 'tours'
+    __tablename__ = "tours"
 
     id = Column(Integer, primary_key=True)
     name = Column(String(255), nullable=False)
@@ -278,10 +317,14 @@ class Tours(db.Model):
     user_id = Column(Integer, ForeignKey("users.id"), nullable=False)
     user = relationship("User", back_populates="tours")
 
-    def __init__(self, name, price, user_id):
+    # Constructor to initialize a new tour
+    def __init__(self, name, image, price, date, user_id):
         self.name = name
         self.price = price
         self.user_id = user_id
+        self.image = image
+        self.date = date
 
+    # String representation of the Tours object
     def __repr__(self):
         return f"Tours(id={self.id}, name={self.name}, price={self.price}, date={self.date})"
--- server/seed.py
+++ server/seed.py
@@ -1,22 +1,31 @@
 from myapp import app, db, bcrypt, mail
-from myapp.models import User, Organization
+from myapp.models import User, Organization, Donation, Beneficiary, Story, Inventory, Tours 
 import secrets
 import string
+from datetime import datetime
 
 def seed_database():
-    print("Hey")
+    print("Seeding the database...")
     with app.app_context():
-        # Delete existing data (optional)
+        # Delete existing data 
         db.session.query(User).delete()
         db.session.query(Organization).delete()
+        db.session.query(Donation).delete()
+        db.session.query(Beneficiary).delete()
+        db.session.query(Story).delete()
+        db.session.query(Inventory).delete()
+        db.session.query(Tours).delete()
 
         # Create a sample user
         user1 = User(
             username="sample_user",
             email="user@example.com",
-            password="sample_password",  
-            role="Admin",  
+            password="sample_password",
+            role="Admin",
         )
+
+        db.session.add(user1)
+        db.session.commit()
 
         # Create a sample organization
         organization1 = Organization(
@@ -25,9 +34,62 @@ def seed_database():
             email="org@example.com",
         )
 
-        # Add the user and organization to the database
-        db.session.add(user1)
         db.session.add(organization1)
+        db.session.commit()
+
+        # Create a sample donation
+        donation1 = Donation(
+            amount=100.0,
+            donor_id=user1.id,
+            organization_id=organization1.id,
+            is_anonymous=False,
+        )
+
+        db.session.add(donation1)
+        db.session.commit()
+        
+        # Create a sample beneficiary
+        beneficiary1 = Beneficiary(
+            name="Sample Beneficiary",
+            age=30,
+            gender="Male",
+            address="123 Sample Street",
+        )
+
+        db.session.add(beneficiary1)
+        db.session.commit()
+
+        # Create a sample story
+        story1 = Story(
+            title="Sample Story",
+            content="This is a sample story content.",
+            published_at=datetime.utcnow(),
+            organization_id=organization1.id,
+        )
+
+        db.session.add(story1)
+        db.session.commit()
+
+        # Create a sample inventory item
+        inventory1 = Inventory(
+            name="Sample Item",
+            quantity=10,
+            beneficiary_id=beneficiary1.id,
+        )
+
+        db.session.add(inventory1)
+        db.session.commit()
+
+        # Create a sample tour
+        tour1 = Tours(
+            name="Sample Tour",
+            image="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.tacdn.com%2Fmedia%2Fattractions-splice-spp-674x446%2F07%2Fc1%2F97%2F0e.jpg&tbnid=gP8VHqXNdoNHrM&vet=12ahUKEwiigreb5p2CAxUdlCcCHacXAjwQMygBegQIARA6..i&imgrefurl=https%3A%2F%2Fwww.viator.com%2Ftours%2FNairobi%2FKibera-Slum-Guided-Day-Tour-from-Nairobi%2Fd5280-21143P25&docid=P2vEY0r2DFC8rM&w=669&h=446&q=kibera%20tourist%20spot&ved=2ahUKEwiigreb5p2CAxUdlCcCHacXAjwQMygBegQIARA6",
+            price=100.0,
+            date=datetime.utcnow(),
+            user_id=user1.id,
+        )
+        
+        db.session.add(tour1)
         db.session.commit()
 
 if __name__ == "__main__":
