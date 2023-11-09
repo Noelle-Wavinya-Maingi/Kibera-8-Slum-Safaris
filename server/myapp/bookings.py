ebecb3c7babc7689a7469cf8a745640cb6a5bee2
Noelle-Wavinya-Maingi <noelle.maingi@student.moringaschool.com>
2023-11-09 13:45:22 +0300

    Added payment gateway for the tours booking

--- server/myapp/bookings.py
+++ server/myapp/bookings.py
@@ -3,8 +3,9 @@ from flask import request
 from flask_restx import Resource, fields
 from flask_mail import Message
 
+# Import necessary models and libraries
 from myapp.models import user_tours, Tours, User
-from myapp import api, db, mail, booking_ns
+from myapp import api, db, mail, booking_ns, stripe
 
 # Define Data Transfer Object for booking information
 booking_info = booking_ns.model(
@@ -14,8 +15,37 @@ booking_info = booking_ns.model(
         "tour_date": fields.Date(
             required=True, description="Date of the tour in YYYY-MM-DD format"
         ),
+        "number_of_persons": fields.Integer(
+            required=True, description="Number of persons on the tour"
+        ),
     },
 )
+
+
+# Function to calculate the total booking amount
+def calculate_booking_amount(tour, number_of_persons):
+    base_price = tour.price
+
+    total_price = base_price * number_of_persons
+
+    return total_price
+
+
+# Function to send an email confirmation to the user
+def send_booking_confirmation_email(
+    user_email, user_username, tour_name, tour_date, booking_amount
+):
+    msg = Message(
+        "Tour Booking Confirmation", sender="noreply@gmail.com", recipients=[user_email]
+    )
+
+    msg.body = (
+        f"Hello {user_username},\n"
+        + f"Thank you for booking the {tour_name} tour for {tour_date}. The total amount is USD{booking_amount}. Enjoy your trip!\n\n"
+        + "Regards,\n"
+        + "Kibera-8 Slum Safaris"
+    )
+    mail.send(msg)
 
 
 @booking_ns.route("/")
@@ -30,12 +60,17 @@ class BookTour(Resource):
         user_id = current_user_id
         tour_name = data.get("tour_name")
         tour_date = data.get("tour_date")
+        number_of_persons = data.get("number_of_persons")
 
+        # Input validation
         if not tour_name:
             return {"message": "Tour name is required"}, 400
 
         if not tour_date:
             return {"message": "Tour date is required"}, 400
+
+        if number_of_persons < 1:
+            return {"message": "Number of persons must be at least 1"}, 400
 
         # Lookup the tour name based on the provided tour_id
         tour = Tours.query.filter_by(name=tour_name).first()
@@ -44,11 +79,20 @@ class BookTour(Resource):
             return {"message": "Tour not found"}, 404
 
         try:
+            # Calculate the booking amount
+            booking_amount = calculate_booking_amount(tour, number_of_persons)
+
+            # Insert booking information into the database
             user_tour_entry = user_tours.insert().values(
                 user_id=user_id, tours_id=tour.id, tour_date=tour_date
             )
             db.session.execute(user_tour_entry)
             db.session.commit()
+
+            # Create a Payment Intent using the Stripe API
+            payment_intent = stripe.PaymentIntent.create(
+                amount=int(booking_amount * 100), currency="usd"
+            )
 
             # Get the user's email from the users table
             user = User.query.get(user_id)
@@ -57,7 +101,7 @@ class BookTour(Resource):
 
             # Send an email to the user
             send_booking_confirmation_email(
-                user_email, user_username, tour.name, tour_date
+                user_email, user_username, tour.name, tour_date, booking_amount
             )
 
             # Return the tour information and date as a JSON response
@@ -66,22 +110,10 @@ class BookTour(Resource):
                 "tour_name": tour.name,
                 "tour_date": tour_date,
                 "tour_price": tour.price,
+                "number_of_persons": number_of_persons,
+                "total_price": booking_amount,
+                "client_secret": payment_intent.client_secret,
             }
             return response_data, 201
         except Exception as e:
             return {"message": "Error while booking the tour", "error": str(e)}, 500
-
-
-# Function to send an email to the user
-def send_booking_confirmation_email(user_email, user_username, tour_name, tour_date):
-    msg = Message(
-        "Tour Booking Confirmation", sender="noreply@gmail.com", recipients=[user_email]
-    )
-
-    msg.body = (
-        f"Hello {user_username},\n"
-        + f"Thank you for booking the {tour_name} tour for {tour_date}. Enjoy your trip!\n\n"
-        + "Regards,\n"
-        + "Kibera-8 Slum Safaris"
-    )
-    mail.send(msg)
